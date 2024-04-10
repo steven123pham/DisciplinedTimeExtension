ef1890aa23cc64b0e139f4d785c52c33cd9ea3ca
steven123pham <91401557+steven123pham@users.noreply.github.com>
2024-04-10 00:33:36 -0500

    extension-ui: Added a play and pause button that switches when clicking

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import PlayCircleRoundedIcon from '@mui/icons-material/PlayCircleRounded';
+import PauseCircleFilledRoundedIcon from '@mui/icons-material/PauseCircleFilledRounded';
+import IconButton from '@mui/material/IconButton';
+import PauseCircleFilledRounded from '@mui/icons-material/PauseCircleFilledRounded';
 
 function App() {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  var togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  }
   return (
     <div className="App">
       <div className="PlayButton">
-        <PlayCircleOutlineIcon/>
+        <IconButton onClick={togglePlay}>
+          {
+            isPlaying ? (
+              <PauseCircleFilledRounded style={{height: 100, width: 100}}/>
+            ) : (
+              <PlayCircleRoundedIcon style={{height: 100, width: 100}}/>
+            )
+          }
+        </IconButton>
       </div>
     </div>
   );
